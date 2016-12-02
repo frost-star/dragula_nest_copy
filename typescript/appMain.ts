9410ea7a8ef1ff28625213a3ced0519b56f03f87
frostar <frostar47@gmail.com>
2016-12-03 08:39:04 +0900

    デザイン調整

--- typescript/appMain.ts
+++ typescript/appMain.ts
@@ -7,13 +7,22 @@ import {ViewChild, Component, Type} from "@angular/core";
       .body {
         padding-left: 151px;
       }
+      .drag-area {
+        position: fixed;
+        top: 0;
+        bottom: 0;
+        right: 0;
+        left: 150px;
+        padding: 20px;
+      }
     `
   ],
   template: `
   <main>
     <sidebar></sidebar>
     <div class="body">
-      <blank></blank>
+      <div class="drag-area" [dragula]='"tool-bag"'>
+      </div>
     </div>
   </main>
   `
